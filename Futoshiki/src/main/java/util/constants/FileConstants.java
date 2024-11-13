9064c572cf6d6c6403d5eb622edaf97f1897066d
AndresHC <foogles05@gmail.com>
2024-11-12 21:26:27 -0600

    Hello World
    Prueba de comit

--- Futoshiki/src/main/java/util/constants/FileConstants.java
+++ Futoshiki/src/main/java/util/constants/FileConstants.java
@@ -6,4 +6,6 @@ public class FileConstants {
     public static final String CURRENT_GAME_FILE = "futoshiki2024juegoactual";
     public static final String TOP10_FILE = "futoshiki2024top10";
     public static final String PLAYERS_FILE = "futoshiki2024players.xml";
+    //Hello world
+
 }
